9d36624e0ed1798c765a87dc5b3a6e19a3032cee
Garv Agarwal <garv51104@gmail.com>
2024-06-30 15:10:59 +0530

    Making New Components

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-        Hello I am Navbar Component
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className='logo'/>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="nav-links">
+        <img src={assets.search_icon} />
+        <div className="nav-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <a href="">Sign In</a>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+      Place order page
+    </div>
+  )
+}
+
+export default PlaceOrder
